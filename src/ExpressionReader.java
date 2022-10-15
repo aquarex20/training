5aa27faf7ee524834aef5ac967e311a9ff452ad5
hmili <78061100+aquarex20@users.noreply.github.com>
2022-10-15 03:36:05 -0400

    Elaboration de la fct

---
+++ src/ExpressionReader.java
@@ -0,0 +1,89 @@
+import java.util.ArrayList;
+import java.util.PriorityQueue;
+import java.util.Queue;
+import java.util.Stack;
+
+public class ExpressionReader {
+    char[] expression;
+    Stack<Integer> pile=new Stack<>();
+    ExpressionReader(char[] calcul){
+        expression=calcul;
+    }
+    ExpressionReader(String calcul){
+        expression= calcul.toCharArray();
+    }
+    public int calculer(char[] expression){
+        Stack<Integer> pileArtificielle= new Stack<>();
+
+        for (char c:expression
+        ) {
+            if (c=='*')
+            {
+                int val1=pileArtificielle.pop();
+                int val2=pileArtificielle.pop();
+
+                pileArtificielle.push(val1 * val2);
+            }
+            if (c=='-')
+            {
+                int val1=pileArtificielle.pop();
+                int val2=pileArtificielle.pop();
+                pileArtificielle.push(val1-val2);
+            }
+            if (c=='+')
+            {
+                int val1=pileArtificielle.pop();
+                int val2=pileArtificielle.pop();
+
+                pileArtificielle.push(val1 + val2);
+            }
+            if (c=='/')
+            {
+                int val1=pileArtificielle.pop();
+                int val2=pileArtificielle.pop();
+
+                pileArtificielle.push(val1 / val2);
+            }
+            if (c=='('){
+                ArrayList<Character> newExpression= new ArrayList<Character>();
+                int compte =0;
+                int resultat=0;
+                boolean compter=false;
+                for (char c1: expression
+                     ) {
+                    if (c1=='('){
+                        compter=true;
+                        compte+=1;
+                    }
+                    if (c1==')'){
+                        if (compte==0) {
+                            char[] newExpression2= new char[newExpression.size()];
+                            for (int i = 0; i <newExpression.size() ; i++) {
+                                newExpression2[i]=newExpression.get(i);
+                            }
+                            resultat=calculer(newExpression2);
+                            break;
+                        }
+                        compte-=1;
+                    }
+                    if (compter)
+                        newExpression.add(c1);
+                }
+                pileArtificielle.push(resultat);
+
+
+            }
+
+            else{
+                pileArtificielle.push(Character.getNumericValue(c));
+            }
+
+        }
+        return pileArtificielle.pop();
+    }
+
+    public int calculer(){
+         return calculer(expression);
+    }
+
+}
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        ExpressionReader exprRead= new ExpressionReader("12+");
+        System.out.println("Valeur de calcul: "+exprRead.calculer());
+        exprRead.calculer();
+
     }
 }
